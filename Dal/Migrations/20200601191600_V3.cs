632fb881826fa2b6024feca9d94a21f0338ee7e5
Mustafa Korkmaz <m.korkmaz@outlook.com>
2020-06-01 22:17:31 +0300

    Add customer table migration

---
+++ Dal/Migrations/20200601191600_V3.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dal.Migrations
+{
+    public partial class V3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RemainingBalance",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<double>(
+                name: "DebtBalance",
+                table: "Customers",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "ReceivableBalance",
+                table: "Customers",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DebtBalance",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "ReceivableBalance",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<double>(
+                name: "RemainingBalance",
+                table: "Customers",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}

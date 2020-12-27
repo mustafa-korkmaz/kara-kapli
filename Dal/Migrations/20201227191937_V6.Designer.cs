42226be588c7b451392391b18202e3239d56f63d
Mustafa Korkmaz <m.korkmaz@outlook.com>
2020-12-27 21:20:43 +0200

    add new migration

--- Dal/Migrations/20201227191937_V6.Designer.cs
+++ Dal/Migrations/20201227191937_V6.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Dal.Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Dal.Migrations
 {
     [DbContext(typeof(BlackCoveredLedgerDbContext))]
-    partial class BlackCoveredLedgerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201227191937_V6")]
+    partial class V6
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Dal/Migrations/20201227191937_V6.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Dal.Migrations
+{
+    public partial class V6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentName",
+                table: "Transactions",
+                maxLength: 70,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttachmentName",
+                table: "Transactions");
+        }
+    }
+}
